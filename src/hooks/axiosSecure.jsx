72bbcd634813d2ef6824eb01386d3ff0af99816f
unknown <mdfaruk01720875872>
2025-08-17 15:47:12 +0600

    added an redeply complite

--- src/hooks/axiosSecure.jsx
+++ src/hooks/axiosSecure.jsx
@@ -2,8 +2,8 @@ import axios from 'axios';
 import React from 'react';
 
 const instance = axios.create({
-    baseURL: 'http://localhost:3000',
-    // baseURL: 'https://assignment-12-server-side-zeta.vercel.app',
+    // baseURL: 'http://localhost:3000',
+    baseURL: 'https://assignment-12-server-side-zeta.vercel.app',
     withCredentials: true
 })
 
